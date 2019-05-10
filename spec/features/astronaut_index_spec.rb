92b2999cfb778d38c1cc58b03affde1a400cd3eb
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-05-10 09:34:33 -0600

    finishes story 2 tests and functionality

--- spec/features/astronaut_index_spec.rb
+++ spec/features/astronaut_index_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 describe 'As a visitor on the astronaut index page' do
   before(:each) do
     @astronaut_1 = Astronaut.create!(name: "Buzz", age: 62, job: "Gangster")
-    @astronaut_2 = Astronaut.create!(name: "Neil", age: 65, job: "Human Relations")
-
+    @astronaut_2 = Astronaut.create!(name: "Neil", age: 64, job: "Human Relations")
+    @astronauts = Astronaut.all
   end
   
   it 'should show each astronaut age, name, and job' do
@@ -17,6 +17,11 @@ describe 'As a visitor on the astronaut index page' do
     expect(page).to have_content(@astronaut_2.name)
     expect(page).to have_content(@astronaut_2.age)
     expect(page).to have_content(@astronaut_2.job)
-    
+  end
+
+  it 'should show the average age of all astronauts' do
+    visit astronauts_path
+
+    expect(page).to have_content("Average Age: #{@astronauts.average_age.to_i}")
   end
 end
--- spec/models/astronaut_spec.rb
+++ spec/models/astronaut_spec.rb
@@ -11,4 +11,15 @@ describe Astronaut, type: :model do
     it { should have_many :astronaut_missions}
     it { should have_many :missions}
   end
+
+  describe 'Class Methods' do
+
+    it '.average_age' do
+      astronaut_1 = Astronaut.create!(name: "Buzz", age: 62, job: "Gangster")
+      astronaut_2 = Astronaut.create!(name: "Neil", age: 64, job: "Human Relations")
+      astronauts = Astronaut.all
+
+      expect(astronauts.average_age.to_i).to eq(63)
+    end
+  end
 end
